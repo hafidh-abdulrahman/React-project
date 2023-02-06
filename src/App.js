dbee2ff98e59476d1e5707c8fa42407a51e4fd6a
Hafidh AbdurRahman <abdulrahmanabdulazeez@gmail.com>
2023-02-06 14:04:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
 function App() {
   return (
-    <h2>React-Project</h2>
-   );
+    <div className="App">
+      <Navbar />;
+      <Form />
+    </div>
+  );
 }
 
 export default App;
